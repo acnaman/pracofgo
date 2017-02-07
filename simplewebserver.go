ac65871ab39f91585d16595fab939ecb7b8e28b3
acnaman <acnadin01@gmail.com>
2017-02-08 07:58:03 +0900

    first commit

---
+++ simplewebserver.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func main(){
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(`
+    `))
+  })
+  // Webサーバの開始
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal("ListenAndServe", err)
+  }
+}
