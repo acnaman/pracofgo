8c1cdec721f7a981ee9f677a2031fceaff6fe188
acnaman <acnadin01@gmail.com>
2017-04-04 01:09:00 +0900

    add chatroom

--- room.go
+++ room.go
@@ -1,5 +1,38 @@
 package main
-type room struct{
-  // forwardは他のクライアントに転送するためのメッセージを保持するチャネル
-  forward chan []byte
+
+type room struct {
+	// forwardは他のクライアントに転送するためのメッセージを保持するチャネル
+	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネル
+	leave chan *client
+	// clientsには在室してる全てのクライアントが保持される
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
