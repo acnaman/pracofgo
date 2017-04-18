09e9d006a6ba7e05da01df4e7d79e0917ab1bf4b
acnaman <acnadin01@gmail.com>
2017-04-18 19:48:49 +0900

    Variable port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,16 +21,19 @@ func (t *templateHandlar) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
 	r := newRoom()
 	http.Handle("/", &templateHandlar{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
 	// Webサーバの開始
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
